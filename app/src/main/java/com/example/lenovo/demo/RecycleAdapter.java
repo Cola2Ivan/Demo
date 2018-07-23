43108cbdcfe6771f16ae3ef1b01ebec3fea5c7f4
Cola2Ivan <1164456239@qq.com>
2018-07-23 16:56:05 +0800

    简单测试Glide和CardView

--- app/src/main/java/com/example/lenovo/demo/RecycleAdapter.java
+++ app/src/main/java/com/example/lenovo/demo/RecycleAdapter.java
@@ -7,54 +7,69 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class RecycleAdapter extends RecyclerView.Adapter {
 
-    private int [] arrDataList = new int[30];
-    private Context context = null;
+    private List<String> imageUrlList;
+    private Context context;
 
-    public RecycleAdapter(Context ctx){
+    RecycleAdapter(Context ctx, List<String> imageUrlList){
         context = ctx;
-        for (int i= 0 ; i< 30; i ++){
-            arrDataList[i] = i;
-        }
+        this.imageUrlList = imageUrlList;
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v= LayoutInflater.from(context).inflate(R.layout.item, parent,false);
-        MyViewHolder vh=new MyViewHolder(v);
-        return vh;
+        return new MyViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
-        ((MyViewHolder)holder).textView.setText("test--" + position);
-        ((MyViewHolder)holder).cardView.setOnClickListener(new View.OnClickListener() {
+    public void onBindViewHolder(@NonNull final RecyclerView.ViewHolder holder, final int position) {
+        MyViewHolder myViewHolder = (MyViewHolder) holder;
+        myViewHolder.textView.setText(String.format(Locale.CHINA, "image %d", holder.getAdapterPosition()) );
+        myViewHolder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context,"点击了" + position,Toast.LENGTH_SHORT).show();
+                Toast.makeText(context,"点击了" + holder.getAdapterPosition(),Toast.LENGTH_SHORT).show();
             }
         });
+        RequestOptions options = new RequestOptions()
+                .placeholder(R.mipmap.ic_launcher)
+                .centerInside()
+                .diskCacheStrategy(DiskCacheStrategy.NONE);
+
+        Glide.with(context).load(imageUrlList.get(position)).apply(options).into(myViewHolder.imageView);
     }
 
     @Override
     public int getItemCount() {
-        return arrDataList.length;
+        return imageUrlList.size();
     }
 
     private class MyViewHolder extends RecyclerView.ViewHolder{
 
-        public TextView textView = null;
-        public CardView cardView = null;
+        private TextView textView;
+        private CardView cardView;
+        private ImageView imageView;
 
-        public MyViewHolder(View itemView) {
+        private MyViewHolder(View itemView) {
             super(itemView);
-            textView = (TextView) itemView.findViewById(R.id.card_view_text);
-            cardView = (CardView) itemView.findViewById(R.id.card_view);
+            textView = itemView.findViewById(R.id.card_view_text);
+            cardView = itemView.findViewById(R.id.card_view);
+            imageView = itemView.findViewById(R.id.card_view_image);
         }
     }
 }
