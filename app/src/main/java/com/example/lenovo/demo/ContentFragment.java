0cf43a21c69bc53d29a5698c84a5c76ad4a8bfaf
Cola2Ivan <1164456239@qq.com>
2018-07-23 17:06:29 +0800

    修改警告问题

--- app/src/main/java/com/example/lenovo/demo/ContentFragment.java
+++ app/src/main/java/com/example/lenovo/demo/ContentFragment.java
@@ -10,20 +10,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContentFragment extends Fragment {
 
     private RecyclerView mRecyleView = null;
+    private RecycleAdapter mAdapter = null;
+    private static List<String> imageUrlList = new ArrayList<>();
+
+    //for test
+    static {
+        imageUrlList.add("https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=4269080372,2540720570&fm=27&gp=0.jpg");
+        imageUrlList.add("https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=84653295,2801014711&fm=27&gp=0.jpg");
+        imageUrlList.add("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3336320333,4115709500&fm=27&gp=0.jpg");
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.content_scrolling,null);
-        mRecyleView = (RecyclerView) view.findViewById(R.id.recyler_list);
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.content_scrolling,container,false);
+        mRecyleView = view.findViewById(R.id.recyler_list);
         LinearLayoutManager manager = new LinearLayoutManager(getContext());
         manager.setOrientation(LinearLayoutManager.VERTICAL);
         mRecyleView.setLayoutManager(manager);
         mRecyleView.setNestedScrollingEnabled(false);
-        mRecyleView.setAdapter(new RecycleAdapter(getContext()));
+        mAdapter = new RecycleAdapter(getContext(), imageUrlList);
+        mRecyleView.setAdapter( mAdapter);
         return view;
     }
 }
